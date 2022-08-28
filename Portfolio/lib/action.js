8c2549938bd06b2d4edf32d1069f0065b918b0a8
Shivam Bhawar <81524682+shivam-bhawar@users.noreply.github.com>
2022-08-28 14:10:08 +0530

    Add files via upload

---
+++ Portfolio/lib/action.js
@@ -0,0 +1,43 @@
+function openEducation() {
+  var x = document.getElementById("education_window");
+  if (x.style.display === "block") 
+  {
+    x.style.display = "none";
+  } else 
+  {
+    x.style.display = "block";
+  }
+}
+
+function openCertificates() {
+  var x = document.getElementById("certificates_window");
+  if (x.style.display === "block") 
+  {
+    x.style.display = "none";
+  } else 
+  {
+    x.style.display = "block";
+  }
+}
+
+function openInterests() {
+  var x = document.getElementById("interests_window");
+  if (x.style.display === "block") 
+  {
+    x.style.display = "none";
+  } else 
+  {
+    x.style.display = "block";
+  }
+}
+
+function openAbout() {
+  var x = document.getElementById("about_window");
+  if (x.style.display === "block") 
+  {
+    x.style.display = "none";
+  } else 
+  {
+    x.style.display = "block";
+  }
+}
